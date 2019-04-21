f460ec13f620d2744602e219f69703b4320b7952
phongphu89 <phphu89@gmail.com>
2019-04-21 21:52:11 +0700

    change file server 08

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/webhook', function(req, res) { // Đây là path để validate tooken
 
  
 
-  res.send(req + " " + req.query);
+  res.send(JSON.stringify(req));
 
   // if (req.query['hub.verify_token'] === VALIDATION_TOKEN) {
   //   res.send(req.query['hub.challenge']);
