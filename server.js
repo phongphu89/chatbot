6a0f54e3f104fca48ba0fc5a2d7d16ad3c8cffb3
phongphu89 <phphu89@gmail.com>
2019-04-21 22:26:11 +0700

    change file server 08

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/webhook', function(req, res) { // Đây là path để validate tooken
   if (req.query['hub.verify_token'] === VALIDATION_TOKEN) {
     res.send(req.query['hub.challenge']);
   }
-  res.send('Error, wrong validation token');
+  res.send('Error, wrong validation token 123');
 });
 
 app.post('/webhook', function(req, res) { // Phần xử lý tin nhắn của người dùng gửi đến
